e1a054a7af684c76175fd3ba57347eaf2644ebac
Dmitry <dima-irk35@mail.ru>
2019-07-26 13:15:59 +0300

    1
    1

--- src/main/hello.go
+++ src/main/hello.go
@@ -28,15 +28,13 @@ import (
 	//"os/signal"
 	//"syscall"
 	//"github.com/gotk3/gotk3/gtk"
-	"time"
-
+	//"time"
 	//"fyne.io/fyne/app"
 	//"fyne.io/fyne/widget"
-	"math/rand"
-	"sync"
-
-	"github.com/gosuri/uiprogress"
-	"github.com/gosuri/uiprogress/util/strutil"
+	//"math/rand"
+	//"sync"
+	//"github.com/gosuri/uiprogress"
+	//"github.com/gosuri/uiprogress/util/strutil"
 )
 
 type Valute struct {
@@ -104,21 +102,21 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Test 1C")
 }
 
-func deploy(app string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	bar := uiprogress.AddBar(len(steps)).AppendCompleted().PrependElapsed()
-	bar.Width = 50
+//func deploy(app string, wg *sync.WaitGroup) {
+//	defer wg.Done()
+//	bar := uiprogress.AddBar(len(steps)).AppendCompleted().PrependElapsed()
+//	bar.Width = 50
 
-	// prepend the deploy step to the bar
-	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		return strutil.Resize(app+": "+steps[b.Current()-1], 22)
-	})
+//	// prepend the deploy step to the bar
+//	bar.PrependFunc(func(b *uiprogress.Bar) string {
+//		return strutil.Resize(app+": "+steps[b.Current()-1], 22)
+//	})
 
-	rand.Seed(500)
-	for bar.Incr() {
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(2000)))
-	}
-}
+//	rand.Seed(500)
+//	for bar.Incr() {
+//		time.Sleep(time.Millisecond * time.Duration(rand.Intn(2000)))
+//	}
+//}
 
 func main() {
 
@@ -542,19 +540,19 @@ func main() {
 	//		time.Sleep(time.Millisecond * 20)
 	//	}
 
-	//-----------------------------------------------------------
+	//----------------Работа с еще одним Прогресс баром-------------------------------------------
 
-	fmt.Println("apps: deployment started: app1, app2")
-	uiprogress.Start()
+	//	fmt.Println("apps: deployment started: app1, app2")
+	//	uiprogress.Start()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go deploy("app1", &wg)
-	wg.Add(1)
-	go deploy("app2", &wg)
-	wg.Wait()
+	//	var wg sync.WaitGroup
+	//	wg.Add(1)
+	//	go deploy("app1", &wg)
+	//	wg.Add(1)
+	//	go deploy("app2", &wg)
+	//	wg.Wait()
 
-	fmt.Println("apps: successfully deployed: app1, app2")
+	//	fmt.Println("apps: successfully deployed: app1, app2")
 
 	//--------------- Конец работа с Прогресс баром -----------------
 
